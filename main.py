9ab4d1ab5e2ffc0b96785a83bc011bd460e41287
Charlie-Steer <gruiz.carlos@gmail.com>
2024-04-26 01:14:51 +0200

    Refactor. Lost block recognition and emptly line between task blocks.

--- main.py
+++ main.py
@@ -27,57 +27,56 @@ def remove_empty_lines(file):
     return lines
 
 
-def get_block_lengths(file):
-        lines = remove_empty_lines(file)
-        block_lenghts = []
-        task_line_count: int = 1
+def get_block_lengths(lines: str):
+    block_lenghts = []
+    task_line_count: int = 1
 
-        for i, line in enumerate(lines):
-            if line.startswith('-'):
-                for j in range(len(lines) - i):
-                    if (i + j + 1) < len(lines) and not lines[i + j + 1].startswith('-'):
-                        j += 1
-                        task_line_count += 1
-                    else:
-                        block_lenghts.append(task_line_count)
-                        task_line_count = 1
-                        break
-        return block_lenghts
+    for i, line in enumerate(lines):
+        if line.startswith('-'):
+            for j in range(len(lines) - i):
+                if (i + j + 1) < len(lines) and not lines[i + j + 1].startswith('-'):
+                    j += 1
+                    task_line_count += 1
+                else:
+                    block_lenghts.append(task_line_count)
+                    task_line_count = 1
+                    break
+    return block_lenghts
 
 
-def print_line_tree_chars(line: str, block_lengths: int, block_index: int):
-    branch_string: str = " "
-    branch_len = 0
+def get_branch_str(lines: str, line_index: int):
 
-    if (line.split('-')[0].startswith(' ')):
+    line = lines[line_index]
+    lines_len = len(lines)
+    tab_num = 0
+
+    # Determine indentation level of task
+    if (line.split('-')[0].endswith(' ')):
         tab_num: int = line.split('-')[0].count('    ')
-    elif (line.split('-')[0].startswith('\t')):
+    elif (line.split('-')[0].endswith('\t')):
         tab_num: int = line.split('-')[0].count('\t')
-    else:
-        return
+
+    # Populate branch string
+    branch_str: str = ""
+    next_line_exists = line_index + 1 < lines_len
     for i in range(tab_num * branch_width_mult):
-        if i == 0 and block_line_index == block_lengths[block_index] - 1:
-            branch_string += "└"
-            #print(' └', end='')
+        if i == 0 and (not next_line_exists or lines[line_index + 1].startswith('-')):
+            branch_str += "  └"
         elif i == 0:
-            branch_string += "├"
-            #print(' ├', end='')
+            branch_str += "  ├"
         else:
-            branch_string += "─"
-            #print('─', end='')
-    branch_string += " "
-    print(branch_string, end="")
-
-    branch_len = len(branch_string)
-    return branch_len
+            branch_str += "─"
+    branch_str += " "
+    return branch_str
 
 
-def print_checkbox(status):
+def get_checkbox(status):
+    checkbox_str = '[ ]'
     if status == Status.DONE:
-        print('[✓]', end=' ');
+        checkbox_str = '[✓]'
     else:
-        print('[ ]', end=' ');
-    return 4
+        checkbox_str = '[ ]'
+    return checkbox_str
 
 
 def strikethrough_text(text):
@@ -99,11 +98,37 @@ def validate_task_line(line: str):
         return False
 
 
-def print_whitespace(number: int, end=''):
+def add_whitespace(number: int):
+    whitespace_str = ''
     for i in range(number):
-        print(' ', end='')
-    print(end=end)
+        whitespace_str += ' '
+    return whitespace_str
 
+def set_status(line: str):
+    status_char = line.split('[')[1][0]
+    if status_char == 'x' or status_char == 'X':
+        return Status.DONE
+    else:
+        return Status.NOT_STARTED
+
+# def get_branch_str():
+#     block_index: int = -1
+#     block_line_index: int = 0
+#     in_subtasks = False
+#
+#     # Print branch lines
+#     if line.startswith('-'):
+#         block_index += 1
+#         block_line_index = 0
+#         if in_subtasks:
+#             in_subtasks = False
+#         print(end = '\n')
+#
+#     elif line.startswith(' ') or line.startswith('\t'):
+#         in_subtasks = True
+#         block_line_index += 1
+#
+#     return get_line_tree_chars(line, block_lengths, block_index, block_line_index)
 
 
 class Status(Enum):
@@ -129,53 +154,46 @@ if __name__ == '__main__':
     #TODO: Column titles are not considered in width
     print_project_name(file_name)
     print('Tasks', end='')
-    print_whitespace(40)
+    add_whitespace(40)
     print('Priority', end='')
-    print_whitespace(20)
+    add_whitespace(20)
     print('Due', end='')
     print(end='\n')
     print_separator_line()
 
 
     with open(file_name, 'r') as file:
-        block_lengths = get_block_lengths(file)
-        block_index: int = -1
-        block_line_index: int = 0
 
-        for index, line in enumerate(file):
-            in_subtasks = False
-            text_field_width = 0
-            branch_len = 0
-            checkbox_len = 4
+        lines = remove_empty_lines(file)
 
-            # Print branch lines
-            if line.startswith('-'):
-                block_index += 1
-                block_line_index = 0
-                if in_subtasks:
-                    in_subtasks = False
-                print(end = '\n')
-
-            elif line.startswith(' ') or line.startswith('\t'):
-                in_subtasks = True
-                block_line_index += 1
-                branch_len = print_line_tree_chars(line, block_lengths, block_index)
-
+        for index, line in enumerate(lines):
 
             is_valid_task_line = validate_task_line(line)
             if is_valid_task_line:
-                if line.split('[')[1][0] == 'x':
-                    status = Status.DONE
-                else:
-                    status = Status.NOT_STARTED
-                checkbox_len = print_checkbox(status)
-                task_name: str = get_task_name(line)
-                print_task(task_name, status)
+                clean_line = ''
+                status = set_status(line)
 
+                branch_str = get_branch_str(lines, index)
+                clean_line += branch_str
+                checkbox_and_task_name_str = ''
+                checkbox_and_task_name_str += get_checkbox(status)
+                checkbox_and_task_name_str += ' '
+                task_name: str = get_task_name(line)
+                checkbox_and_task_name_str += task_name
+
+                if (status == Status.DONE):
+                    clean_line += strikethrough_text(checkbox_and_task_name_str)
+                else:
+                    clean_line += checkbox_and_task_name_str
+
+
+                checkbox_len = 4
+                branch_len = len(branch_str)
                 text_field_width = branch_len + checkbox_len + len(task_name)
                 field_whitespace_len = tasks_column_width - text_field_width
-                print_whitespace(field_whitespace_len, end='|')
+                clean_line += add_whitespace(field_whitespace_len)
+                clean_line += '|'
 
-                print(end="\n")
+                print(clean_line)
 
 
